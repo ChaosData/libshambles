c785bf8c6f336be5c51e08f0d421f10d75702c97
Jeff Dileo <jeff.dileo@nccgroup.trust>
2015-09-11 23:06:47 -0400

    test bin build script update

--- build-test.sh
+++ build-test.sh
@@ -1,3 +1,4 @@
-clang++ -std=c++14 -Wall -Wextra -pedantic -fsanitize=address,undefined -g -o test \
-        tester.cc interception.cc util.cc libforge_socket/libforge_socket.cc
-
+clang++ -std=c++14 -stdlib=libc++ -Wall -Wextra -pedantic -g -o test \
+  tester.ccinterception.cc util.cc libforge_socket/libforge_socket.cc \
+  conntrack/delete.cc conntrack/inject.cc libintercept.cc \
+  -lnetfilter_conntrack
